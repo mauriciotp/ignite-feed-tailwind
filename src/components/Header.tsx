af5d600745f46a95eb38ffc21bc3ff572f3cbce2
mauricio <mauricio.tp_@outlook.com>
2025-03-18 22:04:18 -0300

    feat: create header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,9 @@
+import igniteLogo from '../assets/ignite-logo.svg'
+
 export function Header() {
-  return <header>Ignite Feed</header>
+  return (
+    <header className="flex justify-center bg-gray-800 py-5">
+      <img src={igniteLogo} alt="Logotipo do ignite" className="h-8" />
+    </header>
+  )
 }
